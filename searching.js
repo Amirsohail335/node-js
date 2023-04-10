97dc848cb8ed09b000025dcf3f2e72d70727816e
Amir Sohail <amirsohail335@gmail.com>
2023-04-10 08:32:08 +0530

    committed

--- searching.js
+++ searching.js
@@ -1,8 +1,15 @@
-function linearSearch(arr, x){
-    for(let i = 0 ; i< arr.length; i++){
-        if(arr[i] === x){
-            return i;
-        }
+function linearSearch(arr, x) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === x) {
+      return i;
     }
-    return NaN;
+  }
+  return NaN;
 }
+
+function binarySearch(arr, x) {}
+
+module.exports = {
+  linear: linearSearch,
+    binary: binarySearch,
+};
